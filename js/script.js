d44cf73356748486e3f05aa6cec745206d1011af
Amarildo <amarildos@outloo.it>
2024-01-29 17:51:40 +0100

    called endpoint

--- js/script.js
+++ js/script.js
@@ -2,17 +2,20 @@ console.log("js script ok");
 
 console.log("Vue js ok", Vue);
 
-const root = document.getElementById("myList");
+const { createApp } = Vue;
+const myList = document.getElementById("myList");
 const endpoint = "https://flynn.boolean.careers/exercises/api/random/mail";
 const numOfEmails = 10;
 
-axios.get(endpoint).then((res) => {
-  console.log(res.data);
+const app = createApp({
+  data: () => ({}),
+  created() {
+    axios.get(endpoint).then((res) => {
+      console.log(res.data);
 
-  root.innerText = res.data.response;
+      myList.innerText = res.data.response;
+    });
+  },
 });
 
-// const { createApp } = Vue;
-// const app = createApp({});
-
-// app.mount("#root");
+app.mount("#root");
